4ee5365c3fca73ef2f5d2eff870efdac250fe6a5
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-06-27 05:04:49 +0300

    fix warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Импортируем createRoot из react-dom/client
 import App from './App';
 import './styles/main.css';
 import './i18n'; // Импортируем i18n
@@ -7,9 +7,10 @@ import './i18n'; // Импортируем i18n
 // Добавляем класс dark к body при загрузке страницы
 document.body.classList.add('dark');
 
-ReactDOM.render(
+// Создаем root элемент и рендерим приложение
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
