ddb90603265731badd9cc668375b4171de8a4928
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-06-28 07:54:01 +0300

    update design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import '../styles/about.css';
 import Typewriter from './Typewriter';
+import LanguageSwitcher from './LanguageSwitcher';
+import ThemeSwitcher from './ThemeSwitcher';
 
 const About: React.FC = () => {
     const { t } = useTranslation();
@@ -9,6 +11,10 @@ const About: React.FC = () => {
 
     return (
         <section id="about">
+            <div className="switchers">
+                <LanguageSwitcher />
+                <ThemeSwitcher />
+            </div>
             <h2>{t('about.name')}</h2>
             <p>
                 <b className="neon-dark">
